1018527ac7cf69cb36d854fa6d5467fe68de064c
alwstar <alwis.stark@web.de>
2024-09-08 19:21:02 +0200

    Update server2.bat

--- server2.bat
+++ server2.bat
@@ -1,9 +1,9 @@
 @echo off
 REM Open command line
 start cmd /k "cd /d C:\git\DistributedSystemsChatApp && python server3.py 6001"
-timeout /t 3 /nobreak >nul
+timeout /t 1 /nobreak >nul
 start cmd /k "cd /d C:\git\DistributedSystemsChatApp && python server3.py 6002"
-timeout /t 3 /nobreak >nul
+timeout /t 1 /nobreak >nul
 start cmd /k "cd /d C:\git\DistributedSystemsChatApp && python server3.py 6003"
-timeout /t 3 /nobreak >nul
+timeout /t 1 /nobreak >nul
 start cmd /k "cd /d C:\git\DistributedSystemsChatApp && python server3.py 6004"
