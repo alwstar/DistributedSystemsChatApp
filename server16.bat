34a356bb69576d982a961b67d6e303b75b7e55c6
alwstar <alwis.stark@web.de>
2024-09-09 17:29:45 +0200

    16up

--- server16.bat
+++ server16.bat
@@ -5,5 +5,7 @@ timeout /t 1 /nobreak >nul
 start cmd /k "cd /d C:\git\DistributedSystemsChatApp && python server16.py"
 timeout /t 1 /nobreak >nul
 start cmd /k "cd /d C:\git\DistributedSystemsChatApp && python server16.py"
+timeout /t 16 /nobreak >nul
+start cmd /k "cd /d C:\git\DistributedSystemsChatApp && python client16.py"
 timeout /t 1 /nobreak >nul
 start cmd /k "cd /d C:\git\DistributedSystemsChatApp && python client16.py"
