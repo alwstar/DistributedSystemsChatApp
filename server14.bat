ec15e67dec90e09494d137ca947b975702430ec0
alwstar <alwis.stark@web.de>
2024-09-09 16:05:33 +0200

    14

---
+++ server14.bat
@@ -0,0 +1,7 @@
+@echo off
+REM Open command line
+start cmd /k "cd /d C:\git\DistributedSystemsChatApp && python server14.py"
+timeout /t 1 /nobreak >nul
+start cmd /k "cd /d C:\git\DistributedSystemsChatApp && python server14.py"
+timeout /t 1 /nobreak >nul
+start cmd /k "cd /d C:\git\DistributedSystemsChatApp && python server14.py"
