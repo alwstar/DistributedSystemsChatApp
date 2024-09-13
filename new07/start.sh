4894f295e614182afc30d5d9e9d63daeed3c1622
alwstar <alwis.stark@web.de>
2024-09-13 15:16:05 +0200

    Create start.sh

---
+++ new07/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Navigate to the directory
+cd /path/to/DistributedSystemsChatApp/new06
+
+# Start the first server on port 6001 in a new terminal window
+gnome-terminal -- bash -c "python3 s1.py 6001; exec bash"
+
+# Wait for 2 seconds before starting the next server
+sleep 2
+
+# Start the second server on port 6002 in a new terminal window
+gnome-terminal -- bash -c "python3 c1.py; exec bash"
