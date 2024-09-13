4cb13cb3e68652bf288253762a2df852ff588577
alwstar <alwis.stark@web.de>
2024-09-13 15:53:45 +0200

    Update start.bat

--- new08/start.bat
+++ new08/start.bat
@@ -1,5 +1,5 @@
 @echo off
 REM Open command line
-start cmd /k "cd /d C:\git\DistributedSystemsChatApp\new06 && python s1.py"
+start cmd /k "cd /d C:\git\DistributedSystemsChatApp\new08 && python s1.py"
 timeout /t 2 /nobreak >nul
-start cmd /k "cd /d C:\git\DistributedSystemsChatApp\new06 && python c1.py"
+start cmd /k "cd /d C:\git\DistributedSystemsChatApp\new08 && python c1.py"
