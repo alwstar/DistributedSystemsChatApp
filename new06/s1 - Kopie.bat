3ba91ae638f04dddfd4d5c6f96ea17a0c8f970bd
alwstar <alwis.stark@web.de>
2024-09-13 14:42:00 +0200

    Update s1 - Kopie.bat

--- new06/s1 - Kopie.bat
+++ new06/s1 - Kopie.bat
@@ -1,5 +1,9 @@
 @echo off
 REM Open command line
-start cmd /k "cd /d C:\git\DistributedSystemsChatApp\new03 && python s1.py"
-timeout /t 1 /nobreak >nul
-start cmd /k "cd /d C:\git\DistributedSystemsChatApp\new03 && python c1.py"
+start cmd /k "cd /d C:\git\DistributedSystemsChatApp\new06 && python s1.py 6001"
+timeout /t 2 /nobreak >nul
+start cmd /k "cd /d C:\git\DistributedSystemsChatApp\new06 && python s1.py 6002"
+timeout /t 2 /nobreak >nul
+start cmd /k "cd /d C:\git\DistributedSystemsChatApp\new06 && python s1.py 6003"
+timeout /t 2 /nobreak >nul
+start cmd /k "cd /d C:\git\DistributedSystemsChatApp\new06 && python s1.py 6004"
