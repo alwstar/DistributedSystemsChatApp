df2eb2ddea7636058d39ab756a968003c69cd045
alwstar <alwis.stark@web.de>
2024-09-15 17:58:16 +0200

    Create start 4 servers and 1 client.bat

---
+++ new12/start 4 servers and 1 client.bat
@@ -0,0 +1,11 @@
+@echo off
+REM Open command line
+start cmd /k "cd /d C:\git\DistributedSystemsChatApp\new12 && python s.py"
+timeout /t 1 /nobreak >nul
+start cmd /k "cd /d C:\git\DistributedSystemsChatApp\new12 && python s.py"
+timeout /t 1 /nobreak >nul
+start cmd /k "cd /d C:\git\DistributedSystemsChatApp\new12 && python s.py"
+timeout /t 1 /nobreak >nul
+start cmd /k "cd /d C:\git\DistributedSystemsChatApp\new12 && python s.py"
+timeout /t 1 /nobreak >nul
+start cmd /k "cd /d C:\git\DistributedSystemsChatApp\new12 && python c.py"
